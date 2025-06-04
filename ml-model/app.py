ca92eb13c3ea6aad19d769ef41c64c08562e84c2
Lisa <162413449+lissxa@users.noreply.github.com>
2025-06-04 11:48:42 +0700

    Update app.py

--- ml-model/app.py
+++ ml-model/app.py
@@ -29,13 +29,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-if __name__ == "__main__":
-    import uvicorn
-    import os
-    port = int(os.environ.get("PORT", 8000))  # default ke 8000 [local]
-    uvicorn.run("app:app", host="0.0.0.0", port=port)
-
-
 # Input schema
 class PatientData(BaseModel):
     gender: str
