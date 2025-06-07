bc49efe3eb767de1ba546d920f96c23e94cd2f07
Mariawen <mariastephanie2002@gmail.com>
2025-06-07 13:18:20 +0700

    deploy v4

--- app.py
+++ app.py
@@ -110,6 +110,6 @@ def predict(data: PatientData):
 
 # Jalankan server dengan port dari .env
 if __name__ == "__main__":
-    host = os.getenv("HOST", "127.0.0.1")
-    port = int(os.getenv("PORT", 8000))
+    host = "0.0.0.0"
+    port = 8000
     uvicorn.run("main:app", host=host, port=port, reload=True)
